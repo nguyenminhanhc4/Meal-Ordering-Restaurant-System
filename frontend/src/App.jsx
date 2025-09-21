706ee318f0000c3d2504c5e60113dc6af6942257
nguyen minh anh <nanh2832003@gmail.com>
2025-09-21 12:52:59 +0700

    MA: Tạo frontend react + vite và tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-4 text-center">
+      Chào mừng đến với ShoppyFood
+    </div>
+  );
+}
+
+export default App;
