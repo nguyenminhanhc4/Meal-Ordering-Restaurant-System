0eae47a2be20a0f809faa8ea77252579614eaea9
NVTL2003 <longlollel@gmail.com>
2025-10-26 09:36:53 +0700

    Update MailConfig.java

--- backend/src/main/java/org/example/backend/config/MailConfig.java
+++ backend/src/main/java/org/example/backend/config/MailConfig.java
@@ -13,16 +13,16 @@ public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("sandbox.smtp.mailtrap.io");
-        mailSender.setPort(2525);
-        mailSender.setUsername("3a29e5880676d1");
-        mailSender.setPassword("c3f90d2a190fdb");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true"); // bật log để debug
+//        mailSender.setHost("sandbox.smtp.mailtrap.io");
+//        mailSender.setPort(2525);
+//        mailSender.setUsername("3a29e5880676d1");
+//        mailSender.setPassword("c3f90d2a190fdb");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true"); // bật log để debug
 
         return mailSender;
     }
